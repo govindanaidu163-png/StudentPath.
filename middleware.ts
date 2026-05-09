6034f81c543cfa6d230ec6f07ac8739f4c0ca270
Govinda <your-email@gmail.com>
2026-05-09 14:34:47 +0530

    fixing auth

--- middleware.ts
+++ middleware.ts
@@ -6,61 +6,6 @@ export function middleware(
   request: NextRequest
 ) {
 
-  const token =
-    request.cookies.get(
-      "sb-access-token"
-    );
-
-  const isAuthPage =
-    request.nextUrl.pathname.startsWith(
-      "/auth"
-    );
-
-  const isProtectedRoute =
-    request.nextUrl.pathname.startsWith(
-      "/explore"
-    ) ||
-
-    request.nextUrl.pathname.startsWith(
-      "/dashboard"
-    ) ||
-
-    request.nextUrl.pathname.startsWith(
-      "/admin"
-    );
-
-  /* NOT LOGGED IN */
-
-  if (
-    isProtectedRoute &&
-    !token
-  ) {
-
-    return NextResponse.redirect(
-      new URL(
-        "/auth",
-        request.url
-      )
-    );
-
-  }
-
-  /* LOGGED IN TRYING TO OPEN AUTH */
-
-  if (
-    isAuthPage &&
-    token
-  ) {
-
-    return NextResponse.redirect(
-      new URL(
-        "/explore",
-        request.url
-      )
-    );
-
-  }
-
   return NextResponse.next();
 
 }
@@ -68,15 +13,9 @@ export function middleware(
 export const config = {
 
   matcher: [
-
-    "/auth/:path*",
-
     "/explore/:path*",
-
     "/dashboard/:path*",
-
     "/admin/:path*",
-
   ],
 
 };
